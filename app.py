5fec1061d0514961de1322b9990cd30401fc4e54
krishna5531 <krishnashingala05@gmail.com>
2024-05-30 11:14:02 +0530

    this is my first commit of code in github

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import  Flask , request, render_template
+import requests
+app = Flask(__name__)
+
+@app.route('/homepage')
+def homepage():
+    return render_template("index.html")
+
+@app.route("/weatherapp", methods=['POST', "GET"])
+def get_weather_data():
+    url="https://api.weatherapi.com/v1/current.json"
+
+    p={'key': request.form.get("apiid"),   #here request method/class of flask module is used 
+        'q': request.form.get("city")
+        }
+    response = requests.get(url, params=p)   # here requests module is used 
+    data = response.json()
+    return f"data: {data}"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5002")
