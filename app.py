39aad584183cb88541ee1966156427fc7479faf0
krishna5531 <154584832+krishna5531@users.noreply.github.com>
2024-05-31 09:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import  Flask , request, render_template
 import requests
 app = Flask(__name__)
 
-@app.route('/homepage')
+@app.route('/')
 def homepage():
     return render_template("index.html")
 
